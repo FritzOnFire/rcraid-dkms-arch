864790822c2ca9d5525b5019ad0b50859d9bf848
FritzOnFire <fritz.engl@gmail.com>
2023-03-19 19:07:59 +0200

    fix_missing_h_files cater for more common case

--- fix_missing_h_files.sh
+++ fix_missing_h_files.sh
@@ -1,3 +1,4 @@
 gcc_v=`gcc --version | awk '/gcc/ {print $3}'`
+kver=$(uname -r)
 
-sudo ln -sf /usr/lib/gcc/x86_64-pc-linux-gnu/$gcc_v/include/stdarg.h /usr/src/linux-git/include/.
+sudo ln -sf /usr/lib/gcc/x86_64-pc-linux-gnu/$gcc_v/include/stdarg.h /lib/modules/$kver/build/include/.
